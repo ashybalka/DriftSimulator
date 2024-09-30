a995eb00e6e9b1610190f3a3e3d1239c8e3006c7
Artyom <artemshibalko@gmail.com>
2024-09-30 18:44:34 +0300

    the same

--- Build/ServiceWorker.js
+++ Build/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "IdleClickerCreations-DriftSimulator-0.1.0";
 const contentToCache = [
     "Build/Build.loader.js",
-    "Build/Build.framework.js",
-    "Build/Build.data",
-    "Build/Build.wasm",
+    "Build/Build.framework.js.gz",
+    "Build/Build.data.gz",
+    "Build/Build.wasm.gz",
     "TemplateData/style.css"
 
 ];
