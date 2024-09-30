004cee763d25667d12401f6119095bd323a30aeb
Artyom <artemshibalko@gmail.com>
2024-09-30 12:56:06 +0300

    add skyboxes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,7 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset = new Vector3(0, 5, -10); 
-    public float smoothSpeed = 0.2f; 
+    public float smoothSpeed = 1f; 
 
     void LateUpdate()
     {
